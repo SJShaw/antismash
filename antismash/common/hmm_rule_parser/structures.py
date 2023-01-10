f892d0ea35921acd6a07354986f0bfa9008b2eec
Simon Shaw <SJShaw@users.noreply.github.com>
2023-01-10 13:52:47 +0100

    common.hmm_rule_parser: fix profile hit coordinates

--- antismash/common/hmm_rule_parser/structures.py
+++ antismash/common/hmm_rule_parser/structures.py
@@ -35,7 +35,7 @@ class HMMerHit(ProfileHit):
     @classmethod
     def from_hsp(cls, hsp: HSP, seeds: int) -> "HMMerHit":
         """ Constructs an instance from an HSP """
-        return cls(hsp.hit_id, hsp.query_id, hsp.query_start, hsp.query_end, seeds, hsp.evalue, hsp.bitscore)
+        return cls(hsp.hit_id, hsp.query_id, hsp.hit_start, hsp.hit_end, seeds, hsp.evalue, hsp.bitscore)
 
 
 class DynamicHit(ProfileHit):
